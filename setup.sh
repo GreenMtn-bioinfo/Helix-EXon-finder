d3c29cf9fec5400f10bdc04c615a6b8fb34e06cd
Jonathan Hummel <jhgmbioinfo@gmail.com>
2026-03-09 17:49:21 -0400

    Changed setup script to set conda environment variables such that TensorFlow/CUDA only sees/uses first GPU.

--- setup.sh
+++ setup.sh
@@ -8,8 +8,8 @@ echo "Patching GPU library paths..."
 # Initialize Conda for this subshell
 eval "$(conda shell.bash hook)"
 
-# Activate the environment and permanently map the C++ libraries
+# Activate the environment, permanently map the C++ libraries, and ensure TensorFlow does not use more than the first GPU
 conda activate HEX-finder
-conda env config vars set LD_LIBRARY_PATH=$CONDA_PREFIX/lib:$LD_LIBRARY_PATH
+conda env config vars set LD_LIBRARY_PATH=$CONDA_PREFIX/lib:$LD_LIBRARY_PATH CUDA_VISIBLE_DEVICES=0
 
 echo "Setup complete! Run 'conda activate HEX-finder' to get started."
